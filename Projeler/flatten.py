724eaf2b656f9f5dc8ac0d885c3a6d10768b700f
Fatih Hakan Mehmet ÇAYLIDEMIRCI <cyldmrcfhm@gmail.com>
2021-11-02 21:59:53 +0300

    flatten.py dosyası güncellendi

--- Projeler/flatten.py
+++ Projeler/flatten.py
@@ -4,23 +4,19 @@ def flattenListe(liste):
 
 
     for i in flatListe:
-        if type (i) == list:
-            for a in i:
-                if type(a) == list:
-                    for b in a:
-
-                        if type(b) == list:
-                            for c in b:
-                                düzListe.append(c)
-
-                        else:
-
-                            düzListe.append(b)
-                        
-                else:
-                    düzListe.append(a)
-        else:
+        if type(i) == int or type(i) == float or type(i) == str:
             düzListe.append(i)
+        else:
+            for a in i:
+                if type(a) == int or type(a) == float or type(a) == str:
+                    düzListe.append(a)
+                else:
+                    for b in a:
+                        if type(b) == int or type(b) == float or type(b) == str:
+                            düzListe.append(b)
+                            
+
+
 
 
     print(düzListe)
